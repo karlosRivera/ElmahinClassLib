c92a88e65e969caa18e04b76613d69b1508c55a5
tridipkolkata <tridip2000@gmail.com>
2018-01-03 19:34:56 +0530

    pass date as route data

--- ElmahTest/Controllers/HomeController.cs
+++ ElmahTest/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ElmahTest.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(DateTime? mydata)
         {
 
             MYBal x = new MYBal();
